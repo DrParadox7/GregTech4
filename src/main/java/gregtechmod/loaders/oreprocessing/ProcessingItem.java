a802e85e63a24b62b97e8973e5c13ad1c97a17a3
DrParadox7 <gustavo.caliari@live.com>
2023-07-29 19:08:30 +0200

    Cleanup Ore Registration

--- src/main/java/gregtechmod/loaders/oreprocessing/ProcessingItem.java
+++ src/main/java/gregtechmod/loaders/oreprocessing/ProcessingItem.java
@@ -14,39 +14,53 @@ import net.minecraft.item.ItemStack;
 
 public class ProcessingItem implements IOreRecipeRegistrator {
 
-	public ProcessingItem() {
-		OrePrefixes.item.add(this);
-	}
+    public ProcessingItem() {
+        OrePrefixes.item.add(this);
+    }
 
-	public void registerOre(OrePrefixes aPrefix, List<OreDictEntry> entries) {
-		for (OreDictEntry entry : entries) {
-			if (this.isExecutable(aPrefix, this.getMaterial(aPrefix, entry))) {
-				for (ItemStack aStack : entry.ores) { // TODO fix this shit
-					if (entry.oreDictName.equals("itemManganese")) {
-						GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Manganese, GregTech_API.MATERIAL_UNIT);
-					} else if (entry.oreDictName.equals("itemSalt")) {
-						GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Salt, GregTech_API.MATERIAL_UNIT);
-					} else if (entry.oreDictName.equals("itemMagnesium")) {
-						GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Magnesium, GregTech_API.MATERIAL_UNIT);
-					} else if (!entry.oreDictName.equals("itemPhosphorite") && !entry.oreDictName.equals("itemPhosphorus")) {
-						if (entry.oreDictName.equals("itemSulfur")) {
-							GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Sulfur, GregTech_API.MATERIAL_UNIT);
-						} else if (!entry.oreDictName.equals("itemAluminum") && !entry.oreDictName.equals("itemAluminium")) {
-							if (entry.oreDictName.equals("itemSaltpeter")) {
-								GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Saltpeter, GregTech_API.MATERIAL_UNIT);
-							} else if (entry.oreDictName.equals("itemUranium")) {
-								GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Uranium, GregTech_API.MATERIAL_UNIT);
-							} else if (!entry.oreDictName.equals("itemRubber") && !entry.oreDictName.equals("itemRecord")) {
-								GT_Log.log.warn("Item Name: " + entry.oreDictName + " !!!Unknown Item detected!!! Please report to GregTech Intergalactical for additional compatiblity. This is not an Error, it\'s just an Information.");
-							}
-						} else {
-							GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Aluminium, GregTech_API.MATERIAL_UNIT);
-						}
-					} else {
-						GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Phosphorus, GregTech_API.MATERIAL_UNIT);
-					}
-				}
-			}
-		}
-	}
+    public void registerOre(OrePrefixes aPrefix, List<OreDictEntry> entries) {
+        for (OreDictEntry entry : entries) {
+            if (this.isExecutable(aPrefix, this.getMaterial(aPrefix, entry))) {
+                for (ItemStack aStack : entry.ores) {
+
+                    switch (entry.oreDictName) {
+
+                        case "itemManganese":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Manganese, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemSalt":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Salt, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemMagnesium":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Magnesium, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemPhosphor":
+                        case "itemPhosphorite":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Phosphorus, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemSulfur":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Sulfur, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemSaltpeter":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Saltpeter, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemUranium":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Uranium, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemAluminium":
+                        case "itemAluminum":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Aluminium, GregTech_API.MATERIAL_UNIT);
+                            break;
+                        case "itemRubber":
+                            GT_RecipeRegistrator.registerBasicReverseMaceratingAndSmelting(GT_Utility.copyAmount(1, aStack), Materials.Rubber, GregTech_API.MATERIAL_UNIT);
+                            break;
+
+                        default:
+                            if(GregTech_API.OREDICT_DEBUG_MODE)
+                                GT_Log.log.warn("Item Name: " + entry.oreDictName + " !!!Unknown Item detected!!! Please report to GregTech Intergalactical for additional compatiblity. This is not an Error, it\'s just an Information.");
+                    }
+                }
+            }
+        }
+    }
 }
