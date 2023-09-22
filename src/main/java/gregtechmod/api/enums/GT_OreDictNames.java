ae0c300b4a60e99167f733f28775d2bf4d534899
DrParadox7 <gustavo.caliari@live.com>
2023-09-22 15:37:27 +0200

    Restore Crafting Machine Cover

--- src/main/java/gregtechmod/api/enums/GT_OreDictNames.java
+++ src/main/java/gregtechmod/api/enums/GT_OreDictNames.java
@@ -127,6 +127,7 @@ public enum GT_OreDictNames {
 	craftingSolarPanelHV,
 	craftingSolarPanelLV,
 	craftingSolarPanelMV,
+	craftingWorkBench,
 	craftingSteamTank,
 	craftingSteamUpgrade,
 	craftingSuperconductor,
