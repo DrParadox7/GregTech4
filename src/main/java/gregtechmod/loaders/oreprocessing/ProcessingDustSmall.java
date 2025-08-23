fe65768378efffeb02fe962a19887d124708f21e
DrParadox7 <gustavo.caliari@live.com>
2025-08-23 16:52:03 +0200

    Null Checks for automated recipe entries

--- src/main/java/gregtechmod/loaders/oreprocessing/ProcessingDustSmall.java
+++ src/main/java/gregtechmod/loaders/oreprocessing/ProcessingDustSmall.java
@@ -31,7 +31,11 @@ public class ProcessingDustSmall implements IOreRecipeRegistrator {
 			Materials aMaterial = this.getMaterial(aPrefix, entry);
 			if (this.isExecutable(aPrefix, aMaterial)) {
 				ItemStack ingot;
-				RecipeHandler.executeOnFinish(() -> GameRegistry.addRecipe(new GT_Shapeless_Recipe(GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), new Object[] { entry.oreDictName, entry.oreDictName, entry.oreDictName, entry.oreDictName })));
+				RecipeHandler.executeOnFinish(() -> {
+					ItemStack dust = GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L);
+					if (dust != null)
+						GameRegistry.addRecipe(new GT_Shapeless_Recipe(dust, new Object[] { entry.oreDictName, entry.oreDictName, entry.oreDictName, entry.oreDictName }));
+				});
 				if (!aMaterial.contains(SubTag.NO_SMELTING) && (ingot = GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L)) != null) {
 					if (aMaterial.mBlastFurnaceRequired) {
 						RecipeFactory<?> factory = RecipeMaps.BLAST_FURNACE.factory()
